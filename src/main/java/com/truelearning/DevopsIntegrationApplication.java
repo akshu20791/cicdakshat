0e7295c7fab5178053fc55b9c590b495a22686fc
akshat <akshu20791@gmail.com>
2023-12-14 10:46:39 +0530

    c1

--- src/main/java/com/truelearning/DevopsIntegrationApplication.java
+++ src/main/java/com/truelearning/DevopsIntegrationApplication.java
@@ -1,4 +1,4 @@
-package com.javatechie;
+package com.truelearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to True learning by Akshat";
 	}
 
 	public static void main(String[] args) {
